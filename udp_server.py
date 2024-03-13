1225cd0d667b48a5da1d5b7c6b017a8963f61c62
Rail Neira <r.neira03@ufromail.cl>
2024-03-12 21:19:01 -0300

    Agrega implementación del servidor en UDP

---
+++ udp_server.py
@@ -0,0 +1,48 @@
+import socket
+from utils.rtp import parseBytes
+from utils.db import Saver
+from utils.reloj import Reloj
+
+BUFFER_SIZE = 1160
+DELIMITER = b'\n\n'
+
+
+def start_server(host, port):
+    # Crear objeto Saver con conexión a base de datos
+    saver = Saver()
+
+    # Crear un socket UDP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Enlazar el socket a una dirección y puerto específicos
+    server_socket.bind((host, port))
+
+    print(f"Server listening on {host}:{port}")
+
+    reloj = Reloj()
+
+    while True:
+        data, client_address = server_socket.recvfrom(BUFFER_SIZE)
+        print(f"Received data from {client_address[0]}:{client_address[1]}")
+
+        if not data:
+            continue
+
+        print("Data received: ", len(data))
+        reloj.mostrarFrecuencia()
+
+        # Manejo del delimitador
+        packets = data.split(DELIMITER)
+        for packet in packets[:-1]:
+            try:
+                unpacked_data = parseBytes(packet)
+                saver.save(unpacked_data)
+                reloj.aumentarContador()
+            except Exception as e:
+                # Manejar excepciones
+                print(e)
+
+    # No se cierra el socket en UDP, ya que no hay conexión establecida
+
+# Iniciar el servidor UDP
+start_server('localhost', 8080)
